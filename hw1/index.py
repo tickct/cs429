a9d5ea804bd4ac89dd5c7136052ee3c9a6d5f1c6
tickct <seangraysota@gmail.com>
2017-01-28 18:42:55 -0600

    initial code given

---
+++ hw1/index.py
@@ -0,0 +1,22 @@
+#Python 2.7.3
+import re
+import os
+import collections
+import time
+
+class index:
+	def __init__(self,path):
+
+	def buildIndex(self):
+		#function to read documents from collection, tokenize and build the index with tokens
+		#index should also contain positional information of the terms in the document --- term: [(ID1,[pos1,pos2,..]), (ID2, [pos1,pos2,…]),….]
+		#use unique document IDs
+
+	def and_query(self, query_terms):
+	#function for identifying relevant docs using the index
+
+	def print_dict(self):
+        #function to print the terms and posting list in the index
+
+	def print_doc_list(self):
+	# function to print the documents and their document id
